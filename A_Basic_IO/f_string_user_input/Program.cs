b26a22e0b290c4ccbc1f2370573ec8cc4bfc70eb
Vijaya Malla <mallabiju@gmail.com>
2025-08-19 07:16:08 +0545

    Data types and Control Statement

---
+++ A_Basic_IO/f_string_user_input/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace f_string_user_input;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your first name:");
+        string fname = Console.ReadLine();
+        Console.WriteLine("Enter your last name:");
+        string lname = Console.ReadLine();
+        Console.WriteLine("The full name is {0} {1}.", fname, lname);
+    }
+}
---
+++ B_Class_Object/b_class_object_data_member/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace b_class_object_data_member;
+#nullable disable
+
+class Student
+{
+    int roll;
+    string name;
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        s1.roll = 420;
+        s1.name = "Ram";
+        Console.WriteLine("Roll number = " + s1.roll);
+        Console.WriteLine("Name = " + s1.name);
+    }
+}
