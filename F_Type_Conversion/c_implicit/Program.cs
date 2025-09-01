12f90707a500789b49775fd9efa955256d194744
Vijaya Malla <mallabiju@gmail.com>
2025-09-01 18:53:21 +0545

    new updates

---
+++ F_Type_Conversion/c_implicit/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace c_implicit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte a = 10;
+        int b = a;
+        long c = a;
+
+        /* int b = (int)a;
+        long c = (long)a; */
+
+        Console.WriteLine("Byte a =" + a);
+        Console.WriteLine("Integer b = " + b);
+        Console.WriteLine("Long integer c = " + c);
+    }
+}
---
+++ G_Structure/a_struct/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace a_struct;
+
+struct Student
+{
+    public int roll;
+
+    public void getAge(int roll)
+    {
+        Console.WriteLine("Roll number = " + roll);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Student s1 = new Student();
+        Student s1;
+        s1.roll = 89;
+        s1.getAge(s1.roll);
+    }
+}
