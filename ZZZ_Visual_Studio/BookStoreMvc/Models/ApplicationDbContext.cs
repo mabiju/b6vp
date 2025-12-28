1bfbf36741d5503126d919f0ed80db1d7b1d2443
Vijaya Malla <mallabiju@gmail.com>
2025-12-28 06:38:19 +0545

    MVC Database interaction

---
+++ ZZZ_Visual_Studio/BookStoreMvc/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreMvc.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; } // Represents the Books table
+    }
+}
---
+++ ZZZ_Visual_Studio/BookStoreMvc/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreMvc.Models
+{
+    public class Book
+    {
+        [Key] // Marks this as the primary key
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")] // Validation rule
+        public string? Title { get; set; }
+
+        [Required]
+        public string? Author { get; set; }
+
+        [Range(0.01, 1000, ErrorMessage = "Price must be between 0.01 and 1000")]
+        public decimal Price { get; set; }
+    }
+}
