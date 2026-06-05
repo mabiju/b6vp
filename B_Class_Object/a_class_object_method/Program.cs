e549f19dbb444189d674e4ffcb1cb48be6f8245a
Vijaya Malla <mallabiju@gmail.com>
2026-06-05 18:40:07 +0545

    new updates

---
+++ B_Class_Object/a_class_object_method/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace a_class_object_method;
+
+class Student
+{
+    public void hello()
+    {
+        Console.WriteLine("Good morning friends.");
+    }
+    static void Main(string[] args)
+    {
+        /* creating an object for a class in C#
+        class_name object = new class_name(); */
+        Student s1 = new Student();
+        s1.hello();
+    }
+}
---
+++ B_Class_Object/d_user_inputs_with_methods/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace d_user_inputs_with_methods;
+#nullable disable
+
+class Student
+{
+    int roll;
+    string name;
+
+    public void input()
+    {
+        Console.WriteLine("Enter roll number:");
+        roll = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter name:");
+        name = Console.ReadLine();
+    }
+    public void output()
+    {
+        Console.WriteLine("The roll number is " + roll + " and name is " + name);
+    }
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        s1.input();
+        s1.output();
+    }
+}
