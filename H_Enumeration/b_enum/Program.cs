a7f288d7701d3de5fa89d68b63bd1cf852239edb
Vijaya Malla <mallabiju@gmail.com>
2026-07-10 11:11:46 +0545

    Enum, Partial class, Static keyword and Inheritance

---
+++ H_Enumeration/b_enum/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace b_enum;
+
+class Program
+{
+    enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+    static void Main(string[] args)
+    {
+        var today = Days.Sunday;
+        if (today == Days.Friday || today == Days.Saturday)
+        {
+            Console.WriteLine("Hello friends, let have fun.");
+        }
+        else
+        {
+            Console.WriteLine("Hi all, we need to go for work.");
+        }
+    }
+}
