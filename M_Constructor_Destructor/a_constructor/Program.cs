54bb4c2b64629727aef18e3e47b41ce64d43b39c
Vijaya Malla <mallabiju@gmail.com>
2026-07-15 12:33:48 +0545

    Constructor destructor

---
+++ M_Constructor_Destructor/a_constructor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace a_constructor;
+
+class Student
+{
+    public Student()
+    {
+        Console.WriteLine("Hello Students");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+    }
+}
