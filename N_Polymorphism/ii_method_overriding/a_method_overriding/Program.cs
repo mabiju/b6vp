5f3aab3afa53da5013347804b7ab7fd87d8d154e
Vijaya Malla <mallabiju@gmail.com>
2026-07-16 20:22:46 +0545

    Polymorphism and Interface

---
+++ N_Polymorphism/ii_method_overriding/a_method_overriding/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace a_method_overriding;
+
+class Dad
+{
+    public void hi()
+    {
+        Console.WriteLine("Hello Child.");
+    }
+}
+
+class Child : Dad
+{
+    // public new void hi()
+    public void hi()
+    {
+        Console.WriteLine("Hello Dad.");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Child c1 = new Child();
+        c1.hi();
+    }
+}
