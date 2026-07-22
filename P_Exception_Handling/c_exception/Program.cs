b91b777c544f1266f471eca85e07ad447e6a7cca
Vijaya Malla <mallabiju@gmail.com>
2026-07-22 20:42:46 +0545

    exception

---
+++ P_Exception_Handling/c_exception/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace c_exception;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int[] nums = new int[5];
+            /* nums[3] = 78;
+            Console.WriteLine(nums[3]); */
+            nums[10] = 78;
+            Console.WriteLine(nums[10]);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error found : " + e);
+        }
+    }
+}
