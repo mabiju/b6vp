4b3b3d5e66013bec31eb19ac354e8e2b009323fd
Vijaya Malla <mallabiju@gmail.com>
2026-08-21 17:14:05 +0545

    LINQ

---
+++ Y_LINQ/i_linq_no_of_vowels/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace i_linq_no_of_vowels;
+#nullable disable
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a sentence:");
+        var sentence = Console.ReadLine();
+
+        // using imparative syntax (foreach loop)
+        /* int c = 0;
+        foreach(var letter in sentence.ToUpper())
+        {
+           if(letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U')
+           {
+               c++;
+           }
+        }
+        Console.WriteLine("Number of vowels = {0}", c); */
+
+        // using declarative syntax (LINQ)
+        var vowels = new char[] { 'A', 'E', 'I', 'O', 'U' };
+        var query = from letter in sentence.ToUpper() where vowels.Contains(letter) select letter;
+        var count = query.Count(); 
+        Console.WriteLine("Number of vowels = {0}", count);
+    }
+}
