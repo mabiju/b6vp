21ccb4393ea7fb40e34d32b6666f4acdcf6f0c00
Vijaya Malla <mallabiju@gmail.com>
2026-09-02 12:25:35 +0545

    Remove gitignore restrictions

---
+++ ZZZ_Visual_Studio/A_EntityFramework/a_CodeFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a_CodeFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            StudentContext context = new StudentContext();
+            Student st = new Student()
+            {
+                Roll = 45,
+                Name = "Ram",
+                Address = "Bhaktapur"
+            };
+            context.Students.Add(st);
+            context.SaveChanges();
+            Console.WriteLine("Data inserted successfully.");
+            Console.ReadLine();
+        }
+    }
+}
